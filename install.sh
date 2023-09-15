4d637e77c3bc708414e296378efb1051694fa0c2
Erik Kooijman <erikk@delta-n.nl>
2023-09-15 17:02:55 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install gpg ca-certificates curl apt-transport-https lsb-release gnupg python3-pip
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install terraform
+
+sudo waagent -deprovision+user -force
