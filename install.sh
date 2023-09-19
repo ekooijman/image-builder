9c62b13c15b489ce1a244a40a4c51d1e1054efb4
Erik Kooijman <erikk@delta-n.nl>
2023-09-19 14:45:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,4 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install terraform
 
-sudo waagent -deprovision+user -force
 exit 0
